6f7dd376f3a1768747e9777c6aaf43e28fa45ffc
ISLEMNEDDAR <islam.neddar.pro@gmail.com>
2020-11-22 05:24:59 +0100

    fix : add spin when the loading

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {Fragment, useEffect} from "react";
 import "./css/App.css"
 import Header from "../../component/commun/Header";
 import Content from "../../component/commun/Content";
@@ -7,6 +7,7 @@ import FilmContainer from "./FilmContainer";
 import {useDispatch, useSelector} from "react-redux";
 import {dispatchAction} from "../../services/utils/function.service";
 import filmActions from "../../redux/film/actions";
+import {Space, Spin} from "antd";
 
 function App(){
     const {loading,films} = useSelector(state => state.films)
@@ -17,14 +18,23 @@ function App(){
     return(
         <div className={"layout_container"}>
             <Header/>
-            {!loading && films.length!==0?  (<Content>
-                <div className={"category_select_container boxmargin"}>
-                    <MultiSelect/>
-                </div>
-                <FilmContainer films={films}/>
-            </Content>) : (
-                <div></div>
-                )}
+            <Content>
+                {
+                    !loading && films.length!==0 ? (
+                        <Fragment>
+                                <div className={"category_select_container boxmargin"}>
+                                    <MultiSelect/>
+                                </div>
+                                <FilmContainer films={films}/>
+                        </Fragment>
+                        ) : (
+                        <Space>
+                            <Spin size="large" />
+                        </Space>
+                    )
+                }
+            </Content>
+
         </div>
     )
 }
